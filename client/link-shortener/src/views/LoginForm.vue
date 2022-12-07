cf5c4f0142ee55c05c456a91e7debd087d5e2fd6
reyhanqb <107137535+reyhanqb@users.noreply.github.com>
2022-12-07 11:50:52 +0700

    Update LoginForm.vue
    commit reyhan 1

--- client/link-shortener/src/views/LoginForm.vue
+++ client/link-shortener/src/views/LoginForm.vue
@@ -1,34 +1,77 @@
 <template>
-<div class="card bg-light pt-1 pb-1" style="width: 18rem;">
-    <div class="card-body">
-    <label for="" class="card-text">Username</label><br>
-    <input type="text" placeholder="Username" class="form-control" aria-describedby="emailHelp" v-model="username">
-    <label for="">Password</label><br>
-    <input type="password" name="" id="" class="form-control" placeholder="Password" v-model="password">
-    <br>
-    <button @click="loginSuccess(id)" class="btn btn-primary">Login</button>
-    <p>Dont have an account yet? Register <RouterLink to="/about">here</RouterLink></p>
-  </div>
-</div>
+  <body>
+    <div class="position-absolute top-50 start-50 translate-middle">
+      <div class="card bg-light pt-1 pb-1 p-1 shadow" style="width: 18rem;">
+        <div class="card-body">
+        <label for="" class="card-text">Email</label><br>
+        <input type="email" placeholder="Email" class="form-control" aria-describedby="emailHelp" v-model="email">
+        <label for="">Password</label><br>
+        <input type="password" name="" id="" class="form-control" placeholder="Password" v-model="password">
+        <br>
+        <button @click="loginSuccess(email, password)" class="btn btn-primary">Login</button>
+        <div class="card-text">
+          <p>Dont have an account yet? Register <RouterLink to="/">here</RouterLink></p>
+        </div>
+      </div>
+    </div>
+    </div>    
+  </body>
 </template>
 
 <script>
+import { initializeApp } from "firebase/app"
+import { getAuth,  signInWithEmailAndPassword } from "firebase/auth";
+import axios from 'axios'
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBQ6TioiC86BAcKfleP6QTL0WUiWRRQUGg",
+//   authDomain: "express-crud1.firebaseapp.com",
+//   projectId: "express-crud1",
+//   storageBucket: "express-crud1.appspot.com",
+//   messagingSenderId: "450407551559",
+//   appId: "1:450407551559:web:68a374be04324096a4d762"
+// };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
 export default {
   data () {
     return {
-      username: "",
+      email: "",
       password: "",
       id: 1
     }
   },
   methods: {
-    loginSuccess (id) {
-      this.$router.push({ path: `/user/${id}` });
+    async loginSuccess(email, password){
+      try {
+        const res = await axios.get("http://localhost:8000/user/login", {
+          email: this.email,
+          password: this.password
+
+        }).then((response)=>{
+          signInWithEmailAndPassword(auth, email, password)
+          .then((userCredential) => {
+            // Signed in 
+            const user = userCredential.user;
+            const uid = user.uid
+            console.log(uid)
+            this.$router.push({ path: `/user/${uid}` });
+            // ...
+          console.log(response)
+        })
+        })} catch (error) {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorCode, errorMessage)
+        // ..
+      }
     }
   }
 }
 </script>
-
 
 <style>
 
