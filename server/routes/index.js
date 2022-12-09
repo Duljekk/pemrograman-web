7151502ca9a9e8fc28ef0453a6814d02bc28937b
reyhanqb <107137535+reyhanqb@users.noreply.github.com>
2022-12-09 21:35:16 +0700

    Update index.js
    push api login & register

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
+import { getAuth,  signInWithEmailAndPassword, createUserWithEmailAndPassword } from "firebase/auth";
+import express from 'express';
+import cors from 'cors';
+import { db } from '../conf/db.js'
+import { auth } from '../conf/config.js'
+
 const app = require("../firebase/fb_connect");
 const { getDatabase, ref, child, get } = require("firebase/database");
 const dbRef = ref(getDatabase(app));
@@ -24,31 +30,52 @@ router.get("/:code", (req, res) => {
 });
 
 app.post("/user/register", (req, res)=>{
+    const users = db.collection("userInfo")
+    let email = req.body.email
+    let password = req.body.password
     try {
-        users.add({
-            email: req.body.email,
-            password: req.body.password
+        const auth = getAuth();
+        createUserWithEmailAndPassword(auth, email, password)
+        .then((userCredential)=>{
+            const user = userCredential.user
+            const uid = user.uid
+            res.send(uid)
         })
-        res.send({
-            status: true,
-            message: "Succesfully created user"
-        })
+        // users.add({
+        //     email: req.body.email,
+        //     password: req.body.password
+        // })
+        // res.send({
+        //     status: true,
+        //     message: "Succesfully created user"
+        // })
     }
     catch (e) {
         console.log(e)
     }
 })
 
-app.get("/user/login", (req, res)=>{
+app.post("/user/login", (req, res)=>{
+    const users = db.collection("userInfo")
+    let email = req.body.email
+    let password = req.body.password
     try {
-        let userData = []
-        users.get().then((querySnapshot)=>{
-            querySnapshot.forEach((doc) => {
-                let id = doc.id
-                userData.push({ id, ...doc.data()})
-            })
-            res.send(userData)
-        })   
+        signInWithEmailAndPassword(auth, email, password)
+          .then((userCredential) => {
+            // Signed in 
+            const user = userCredential.user;
+            const uid = user.uid
+            console.log(uid)
+            res.send(uid)
+          })
+        // let userData = []
+        // users.get().then((querySnapshot)=>{
+        //     querySnapshot.forEach((doc) => {
+        //         let id = doc.id
+        //         userData.push({ id, ...doc.data()})
+        //     })
+        //     res.send(userData)
+        // })   
     }
     catch (e){
         console.log(e)
