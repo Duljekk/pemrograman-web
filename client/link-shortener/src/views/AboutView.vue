5d7287087ab4147bf9771a7fd115bca57df2ae22
reyhanqb <107137535+reyhanqb@users.noreply.github.com>
2022-12-11 00:19:08 +0700

    Update AboutView.vue

--- client/link-shortener/src/views/AboutView.vue
+++ client/link-shortener/src/views/AboutView.vue
@@ -1,4 +1,63 @@
-async register() {
+<template>
+  <body>
+    <main>
+      <div class="position-absolute top-50 start-50 translate-middle">
+        <div class="card bg-light pt-1 pb-1 p-1 shadow-lg" style="width: 18rem;">
+          <div class="card-body">
+          <label for="" class="card-text">Email</label><br>
+          <input type="email" placeholder="Email" class="form-control" aria-describedby="emailHelp" v-model="email" v-bind:required="email">
+          <label for="">Password</label><br>
+          <input type="password" name="" id="" class="form-control" placeholder="Password" v-model="password" v-bind:required="password">
+          <div v-show="credMissing">
+            <p>Please enter the valid credentials.</p>
+          </div>
+          <div v-show="emailTaken">
+            <p>Email is already in use</p>
+          </div>
+          <div class="pt-3 left">
+            <button @click="register()" class="btn btn-primary">Register</button>
+          </div>
+          <div class="formSubmitted" v-show="isComplete">
+            <br>
+            <p>Login </p> <router-link to="/about">here.</router-link>
+          </div>
+        </div>
+      </div>
+      </div>
+    </main>
+  </body>
+</template>
+
+<script>
+// import { initializeApp } from "firebase/app"
+// import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import axios from 'axios'
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBQ6TioiC86BAcKfleP6QTL0WUiWRRQUGg",
+//   authDomain: "express-crud1.firebaseapp.com",
+//   projectId: "express-crud1",
+//   storageBucket: "express-crud1.appspot.com",
+//   messagingSenderId: "450407551559",
+//   appId: "1:450407551559:web:68a374be04324096a4d762"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const auth = getAuth(app);
+
+export default {
+data() {
+  return {
+    email : "",
+    password : "",
+    isComplete: false,
+    credMissing: false,
+    emailTaken: false
+    }
+  },
+  methods: {
+    async register() {
     try {
         const res = await axios.post("http://localhost:8000/user/register", {
           email: this.email,
@@ -45,3 +104,4 @@ async register() {
     }
   }
 }
+</script>
