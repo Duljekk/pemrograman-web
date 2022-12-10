569babc23885b5d6c30afd39118e634ec3b57010
reyhanqb <107137535+reyhanqb@users.noreply.github.com>
2022-12-10 22:43:43 +0700

    Update LoginForm.vue

--- client/link-shortener/src/views/LoginForm.vue
+++ client/link-shortener/src/views/LoginForm.vue
@@ -1,77 +1,100 @@
 <template>
   <body>
-    <div class="position-absolute top-50 start-50 translate-middle">
-      <div class="card bg-light pt-1 pb-1 p-1 shadow" style="width: 18rem;">
-        <div class="card-body">
-        <label for="" class="card-text">Email</label><br>
-        <input type="email" placeholder="Email" class="form-control" aria-describedby="emailHelp" v-model="email">
-        <label for="">Password</label><br>
-        <input type="password" name="" id="" class="form-control" placeholder="Password" v-model="password">
-        <br>
-        <button @click="loginSuccess(email, password)" class="btn btn-primary">Login</button>
-        <div class="card-text">
-          <p>Dont have an account yet? Register <RouterLink to="/">here</RouterLink></p>
+    <main>
+      <div class="position-absolute top-50 start-50 translate-middle">
+        <div class="card bg-light pt-1 pb-1 p-1 shadow-lg" style="width: 18rem;">
+          <div class="card-body">
+          <label for="" class="card-text">Email</label><br>
+          <input type="email" placeholder="Email" class="form-control" aria-describedby="emailHelp" v-model="email" v-bind:required="email">
+          <label for="">Password</label><br>
+          <input type="password" name="" id="" class="form-control" placeholder="Password" v-model="password" v-bind:required="password">
+          <div v-show="credMissing">
+            <p>Please enter the valid credentials.</p>
+          </div>
+          <div class="pt-3 left">
+            <button @click="register()" class="btn btn-primary">Register</button>
+          </div>
+          <div class="formSubmitted" v-show="isComplete">
+            <br>
+            <p>Login </p> <router-link to="/about">here.</router-link>
+          </div>
         </div>
       </div>
-    </div>
-    </div>    
+      </div>
+    </main>
   </body>
 </template>
 
 <script>
 import { initializeApp } from "firebase/app"
-import { getAuth,  signInWithEmailAndPassword } from "firebase/auth";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 import axios from 'axios'
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyBQ6TioiC86BAcKfleP6QTL0WUiWRRQUGg",
-//   authDomain: "express-crud1.firebaseapp.com",
-//   projectId: "express-crud1",
-//   storageBucket: "express-crud1.appspot.com",
-//   messagingSenderId: "450407551559",
-//   appId: "1:450407551559:web:68a374be04324096a4d762"
-// };
+const firebaseConfig = {
+  apiKey: "AIzaSyBQ6TioiC86BAcKfleP6QTL0WUiWRRQUGg",
+  authDomain: "express-crud1.firebaseapp.com",
+  projectId: "express-crud1",
+  storageBucket: "express-crud1.appspot.com",
+  messagingSenderId: "450407551559",
+  appId: "1:450407551559:web:68a374be04324096a4d762"
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
 export default {
-  data () {
-    return {
-      email: "",
-      password: "",
-      id: 1
+data() {
+  return {
+    email : "",
+    password : "",
+    isComplete: false,
+    credMissing: false
     }
   },
   methods: {
-    async loginSuccess(email, password){
-      try {
-        const res = await axios.post("http://localhost:8000/user/login", {
+    async register() {
+    try {
+        const res = await axios.post("http://localhost:8000/user/register", {
           email: this.email,
-          password: this.password,
+          password: this.password
         }).then((response)=>{
+          console.log(response.data)
+          if(response.data.code != null){
+            this.$router.push("/")
+            this.credMissing = true
+          }
+          // else if(response.data.code == 'auth/email-already-exists'){
+          //   this.$router.push("/about")
+          //   this.emailExist = true
+          // }
+          else {
             console.log(response)
-            // Signed in 
-            // const user = userCredential.user;
-            const uid = response.data
-            console.log(uid)
-            localStorage.setItem('uid', uid)
-            this.$router.push({ path: `/user/${uid}` });
-            // ...
-          console.log(response)
-        })
-        } catch (error) {
+              // Signed in 
+              const uid = response.data
+              console.log(uid)
+              if(this.email !== null && this.password !== null){
+              this.isComplete = true
+              }
+              else if (this.email === "" && this.password === ""){
+                console.log('tes')
+                this.emailMissing = true
+                this.passwordMissing = true
+              }
+            console.log(response)
+          }
+          }
+        )}
+        catch (error) {
         const errorCode = error.code;
         const errorMessage = error.message;
         console.log(errorCode, errorMessage)
         // ..
       }
+    },
+    getDetails() {
+      console.log('Successfully added')
     }
   }
 }
 </script>
-
-<style>
-
-</style>
